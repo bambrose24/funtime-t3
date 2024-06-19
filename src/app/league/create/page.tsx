0d520881f6f6ffb6216f0e05b47edab2bf99c60b
Bob Ambrose <bambrose24@gmail.com>
2024-06-18 22:20:21 -0400

    create league page with buttons i guess

--- src/app/league/create/page.tsx
+++ src/app/league/create/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+import { useState } from "react";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "~/components/ui/card";
+
 export default function CreateLeaguePage() {
-  return <></>;
+  const [stepIdx, setStepIdx] = useState(0);
+
+  const back = () => {
+    setStepIdx((prev) => Math.max(0, prev - 1));
+  };
+  const next = () => {
+    setStepIdx((prev) => prev + 1);
+  };
+
+  return (
+    <div className="col-span-12 flex md:col-span-8 md:col-start-3">
+      <Card className="w-full">
+        <CardHeader className="flex w-full justify-center">
+          Create a League
+        </CardHeader>
+        <CardContent>You&apos;re on step {stepIdx}</CardContent>
+        <CardFooter className="flex w-full justify-between">
+          {stepIdx > 0 ? (
+            <Button variant="secondary" onClick={back}>
+              Back
+            </Button>
+          ) : (
+            <div />
+          )}
+          <Button onClick={next}>Continue</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
