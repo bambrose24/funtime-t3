d640eae894c73a9d217265127191b6715859c2e5
Bob Ambrose <bambrose24@gmail.com>
2024-06-29 10:09:31 -0400

    remove import:

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,6 +1,5 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
-import { COOKIE_TIME_MS } from "./types";
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
