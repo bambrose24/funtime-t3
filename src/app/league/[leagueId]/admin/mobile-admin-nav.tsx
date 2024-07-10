b08d083bad847ab7a6822b0e01f9bc578b35bb70
Bob Ambrose <bambrose24@gmail.com>
2024-07-09 23:53:22 -0400

    fix default

--- src/app/league/[leagueId]/admin/mobile-admin-nav.tsx
+++ src/app/league/[leagueId]/admin/mobile-admin-nav.tsx
@@ -9,12 +9,12 @@ export function MobileAdminNav({ leagueId }: { leagueId: number }) {
   const router = useRouter();
   const pathname = usePathname();
   const baseHref = `/league/${leagueId}/admin`;
+  const defaultValue =
+    getNavOptions(baseHref).find((opt) => pathname === opt.href)?.id ?? "";
+
   return (
     <Tabs
-      defaultValue={
-        getNavOptions(baseHref).find((opt) => pathname.includes(opt.href))
-          ?.id ?? ""
-      }
+      defaultValue={defaultValue}
       onValueChange={(tabHrefValue) => {
         router.push(tabHrefValue);
       }}
@@ -24,7 +24,7 @@ export function MobileAdminNav({ leagueId }: { leagueId: number }) {
         {getNavOptions(baseHref).map((opt, idx) => {
           return (
             <Link href={opt.href} key={idx} className="flex-grow">
-              <TabsTrigger value={opt.href} className="w-full">
+              <TabsTrigger value={opt.id} className="w-full">
                 {opt.display}
               </TabsTrigger>
             </Link>
