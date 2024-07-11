be8ede13f63ef950972bc65008079a1af54f390a
Bob Ambrose <bambrose24@gmail.com>
2024-07-11 09:19:38 -0400

    update prisma schema

--- src/generated/prisma-client/edge.js
+++ src/generated/prisma-client/edge.js
@@ -347,7 +347,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -348,7 +348,6 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
