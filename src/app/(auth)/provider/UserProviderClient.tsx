b19b2529d3f45b9b7100c0b78614ff59b231a51a
Bob Ambrose <bambrose24@gmail.com>
2024-08-22 09:48:30 -0400

    add posthog

--- src/app/(auth)/provider/UserProviderClient.tsx
+++ src/app/(auth)/provider/UserProviderClient.tsx
@@ -2,6 +2,18 @@
 
 import { clientApi } from "~/trpc/react";
 import { type RouterOutputs } from "~/trpc/types";
+import posthog from "posthog-js";
+import { PostHogProvider, usePostHog } from "posthog-js/react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? "", {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "",
+    person_profiles: "identified_only",
+    capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+  });
+}
 
 type Props = {
   data: RouterOutputs["session"]["current"];
@@ -12,5 +24,30 @@ export function UserProviderClient({ children, data: initialData }: Props) {
   clientApi.session.current.useQuery(undefined, {
     initialData,
   });
-  return <>{children}</>;
+  return (
+    <PostHogProvider client={posthog}>
+      {children}
+      <PostHogPageView />
+    </PostHogProvider>
+  );
+}
+
+export default function PostHogPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthogClient = usePostHog();
+  useEffect(() => {
+    // Track pageviews
+    if (pathname && posthogClient) {
+      let url = window.origin + pathname;
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+      posthogClient.capture("$pageview", {
+        $current_url: url,
+      });
+    }
+  }, [pathname, searchParams, posthogClient]);
+
+  return null;
 }
