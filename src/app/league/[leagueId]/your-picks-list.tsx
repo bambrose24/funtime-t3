e018734367732b78ab65a737334c7c39fc3ea541
Bob Ambrose <bambrose24@gmail.com>
2024-09-09 12:12:45 -0400

    show score in the view my picks drawer

--- src/app/league/[leagueId]/your-picks-list.tsx
+++ src/app/league/[leagueId]/your-picks-list.tsx
@@ -6,6 +6,7 @@ import { useDictify } from "~/utils/hooks/useIdToValMemo";
 import { cn } from "~/lib/utils";
 import { Button } from "~/components/ui/button";
 import { type RouterOutputs } from "~/trpc/types";
+import { Card } from "~/components/ui/card";
 
 type Props = {
   myPicks: RouterOutputs["league"]["picksSummary"][number];
@@ -148,7 +149,7 @@ export function CompactYourPicksList(props: Props) {
   const gameIdToGame = useDictify(games, (g) => g.gid);
 
   return (
-    <div className="grid grid-cols-2 gap-x-8 gap-y-0.5">
+    <div className="grid grid-cols-2 gap-x-8 gap-y-1.5">
       {myPicks?.picks?.map((p, idx) => {
         const game = gameIdToGame.get(p.gid);
         if (!game) {
@@ -161,6 +162,9 @@ export function CompactYourPicksList(props: Props) {
         const isSimulated = p.gid in props.simulatedGames;
         const simulatedWinner = props.simulatedGames[p.gid];
         const gameWinner = simulatedWinner ?? game.winner;
+
+        const gameIsTiebreaker = game.is_tiebreaker;
+        const pickScore = p.score;
 
         const status: "simulated" | "empty-state" | "correct" | "wrong" =
           isSimulated
@@ -182,64 +186,73 @@ export function CompactYourPicksList(props: Props) {
               idx % 2 === 0 ? "justify-end" : "justify-start",
             )}
           >
-            <div
-              className={cn(
-                "grid w-[120px] grid-cols-5 flex-row items-center justify-between rounded-xl border-2 border-transparent p-0.5",
-                status === "simulated" && "border-simulated",
-              )}
-            >
-              <Button
-                variant="ghost"
-                size="sm"
-                onClick={() => {
-                  selectGame(p.gid, awayTeam.teamid);
-                }}
+            <Card>
+              <div
                 className={cn(
-                  "col-span-2 flex flex-row items-center justify-center rounded-lg p-1",
-                  status === "simulated" &&
-                    choseAway &&
-                    (gameWinner === awayTeam.teamid
-                      ? "border-2 border-correct"
-                      : "border-2 border-wrong"),
-                  status === "correct" &&
-                    choseAway &&
-                    "border-2 border-correct",
-                  status === "wrong" && choseAway && "border-2 border-wrong",
-                  status === "empty-state" &&
-                    choseAway &&
-                    "border-2 border-pending",
+                  "grid w-[120px] grid-cols-5 flex-row items-center justify-between rounded-xl border-2 border-transparent p-0.5",
+                  status === "simulated" && "border-simulated",
                 )}
               >
-                <Text.Small>{awayTeam.abbrev}</Text.Small>
-              </Button>
-              <div className="col-span-1 flex flex-row justify-center">
-                <Text.Small>@</Text.Small>
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  onClick={() => {
+                    selectGame(p.gid, awayTeam.teamid);
+                  }}
+                  className={cn(
+                    "col-span-2 flex flex-row items-center justify-center rounded-lg p-1",
+                    status === "simulated" &&
+                      choseAway &&
+                      (gameWinner === awayTeam.teamid
+                        ? "border-2 border-correct"
+                        : "border-2 border-wrong"),
+                    status === "correct" &&
+                      choseAway &&
+                      "border-2 border-correct",
+                    status === "wrong" && choseAway && "border-2 border-wrong",
+                    status === "empty-state" &&
+                      choseAway &&
+                      "border-2 border-pending",
+                  )}
+                >
+                  <Text.Small>{awayTeam.abbrev}</Text.Small>
+                </Button>
+                <div className="col-span-1 flex flex-row justify-center">
+                  <Text.Small>@</Text.Small>
+                </div>
+                <Button
+                  variant="ghost"
+                  size="sm"
+                  onClick={() => {
+                    selectGame(p.gid, homeTeam.teamid);
+                  }}
+                  className={cn(
+                    "col-span-2 flex flex-row items-center justify-center rounded-lg p-1",
+                    status === "simulated" &&
+                      choseHome &&
+                      (gameWinner === homeTeam.teamid
+                        ? "border-2 border-correct"
+                        : "border-2 border-wrong"),
+                    status === "correct" &&
+                      choseHome &&
+                      "border-2 border-correct",
+                    status === "wrong" && choseHome && "border-2 border-wrong",
+                    status === "empty-state" &&
+                      choseHome &&
+                      "border-2 border-pending",
+                  )}
+                >
+                  <Text.Small>{homeTeam.abbrev}</Text.Small>
+                </Button>
+                {gameIsTiebreaker === true ? (
+                  <div className="col-span-5 flex items-center justify-center">
+                    <Text.Small className="text-muted-foreground">
+                      Score: {pickScore ?? 0}
+                    </Text.Small>
+                  </div>
+                ) : null}
               </div>
-              <Button
-                variant="ghost"
-                size="sm"
-                onClick={() => {
-                  selectGame(p.gid, homeTeam.teamid);
-                }}
-                className={cn(
-                  "col-span-2 flex flex-row items-center justify-center rounded-lg p-1",
-                  status === "simulated" &&
-                    choseHome &&
-                    (gameWinner === homeTeam.teamid
-                      ? "border-2 border-correct"
-                      : "border-2 border-wrong"),
-                  status === "correct" &&
-                    choseHome &&
-                    "border-2 border-correct",
-                  status === "wrong" && choseHome && "border-2 border-wrong",
-                  status === "empty-state" &&
-                    choseHome &&
-                    "border-2 border-pending",
-                )}
-              >
-                <Text.Small>{homeTeam.abbrev}</Text.Small>
-              </Button>
-            </div>
+            </Card>
           </div>
         );
       })}
