aac564f6033d2a36c387dd0f8159a7ebade28f84
Bob Ambrose <bambrose24@gmail.com>
2025-07-14 21:49:18 -0400

    working fresh expo app with react 19

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,30 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const { FileStore } = require("metro-cache");
+const path = require("path");
+
+const config = getDefaultConfig(__dirname);
+
+// Find the project root
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, "../..");
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [monorepoRoot];
+
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, "node_modules"),
+  path.resolve(monorepoRoot, "node_modules"),
+];
+
+// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+config.resolver.disableHierarchicalLookup = true;
+
+// 4. Use turborepo to restore the cache when possible
+config.cacheStores = [
+  new FileStore({
+    root: path.join(monorepoRoot, "node_modules/.cache/metro"),
+  }),
+];
+
+module.exports = config;
