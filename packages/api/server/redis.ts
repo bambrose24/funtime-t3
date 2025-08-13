0c99914fd0e49529954508dad9abfa5c35878cfe
Bob Ambrose <bambrose24@gmail.com>
2025-08-12 22:27:33 -0400

    dns for redis on railway

--- packages/api/server/redis.ts
+++ packages/api/server/redis.ts
@@ -2,7 +2,9 @@ import Redis from "ioredis";
 import { env } from "~/env";
 import { getLogger } from "~/utils/logging";
 
-const url = env.REDIS_URL ?? "redis://localhost:6379";
+const url = env.REDIS_URL
+  ? `${env.REDIS_URL}?family=1`
+  : "redis://localhost:6379";
 
 export const redis = new Redis(url, {
   maxRetriesPerRequest: 3,
