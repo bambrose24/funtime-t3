b4578e1eb77f6f3c16b67c3453089a954f7751dc
Bob Ambrose <bambrose24@gmail.com>
2025-08-12 22:10:30 -0400

    nicer logs

--- packages/api/server/redis.ts
+++ packages/api/server/redis.ts
@@ -2,15 +2,17 @@ import Redis from "ioredis";
 import { env } from "~/env";
 import { getLogger } from "~/utils/logging";
 
-export const redis = new Redis(env.REDIS_URL ?? "redis://localhost:6379", {
+const url = env.REDIS_URL ?? "redis://localhost:6379";
+
+export const redis = new Redis(url, {
   maxRetriesPerRequest: 3,
   lazyConnect: true,
 });
 
 redis.on("error", (err) => {
-  getLogger().error(`Redis connection error: ${err.message}, ${err.stack}`);
+  getLogger().error(`❌ Redis connection error: ${err.message}, ${err.stack}`);
 });
 
 redis.on("connect", () => {
-  getLogger().info("Redis connected");
+  getLogger().info(`✅ Redis connected to ${url}`);
 });
